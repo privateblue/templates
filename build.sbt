11988f5aaaecd3f5060a11f7ec207647613a018e
Mikolaj Szabo <savingprivateblue@gmail.com>
2018-04-16 12:01:16 +0200

    Simple Scalajs UI

--- build.sbt
+++ build.sbt
@@ -2,8 +2,11 @@ name := "language"
 
 scalaVersion := "2.12.4"
 
+enablePlugins(ScalaJSPlugin)
+
 libraryDependencies ++= Seq(
-  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0",
-  "org.parboiled" %% "parboiled" % "2.1.4",
-  "org.typelevel" %% "cats-core" % "1.0.1"
+  "org.scala-lang.modules" %%% "scala-parser-combinators" % "1.1.0",
+  "org.parboiled" %%% "parboiled" % "2.1.4",
+  "com.chuusai" %%% "shapeless" % "2.3.3",
+  "org.typelevel" %%% "cats-core" % "1.0.1"
 )
--- utils.scala
+++ utils.scala
@@ -1,11 +1,22 @@
 package language
 
 import scala.io.Source
+import scala.scalajs.js.annotation._
 
-object ExpressionRepl {
-  def main(args: Array[String]): Unit = eval()
+object UI {
+  @JSExportTopLevel("render")
+  def render(source: String): String = {
+    val result = for {
+      terms <- TermParser.parse(source)
+      rendered <- TermEvaluator.compile(terms, ExpressionEvaluator.EmptyContext)
+    } yield rendered
+    result match {
+      case Right(r) => r
+      case Left(err) => err.msg
+    }
+  }
 
-  def eval(context: ExpressionEvaluator.Context = ExpressionEvaluator.EmptyContext, count: Int = 0): Unit = {
+  def repl(context: ExpressionEvaluator.Context = ExpressionEvaluator.EmptyContext, count: Int = 0): Unit = {
     val str = scala.io.StdIn.readLine("> ")
     if (str != "quit") for {
       parsed <- ExpressionParser.parse(str)
@@ -18,17 +29,10 @@ object ExpressionRepl {
       name = s"res$count"
       _ = println(s"$name := $parsed ( = $result)")
       nctx = ExpressionEvaluator.put(name, parsed, context)
-    } yield eval(nctx, count + 1)
-  }
-}
-
-object FileProcessor {
-  def main(args: Array[String]): Unit = args.toList match {
-    case filename :: _ => process(filename)
-    case _ => println("File name missing")
+    } yield repl(nctx, count + 1)
   }
 
-  def process(filename: String): Unit = {
+  def compileFile(filename: String): Unit = {
     val source = Source.fromFile(filename).getLines.mkString("\n")
     println(source)
     println
@@ -37,7 +41,7 @@ object FileProcessor {
       rendered <- TermEvaluator.compile(terms, ExpressionEvaluator.EmptyContext)
     } yield rendered
     result match {
-      case Right(r: String) => println(r)
+      case Right(r) => println(r)
       case Left(err) => println("An error occured")
     }
   }
