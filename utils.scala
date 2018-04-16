8096fcbc5004425b48f2dae101da6e4f96ef927c
Mikolaj Szabo <savingprivateblue@gmail.com>
2018-04-16 12:02:14 +0200

    Get rid of command-line utils for now

--- utils.scala
+++ utils.scala
@@ -15,34 +15,4 @@ object UI {
       case Left(err) => err.msg
     }
   }
-
-  def repl(context: ExpressionEvaluator.Context = ExpressionEvaluator.EmptyContext, count: Int = 0): Unit = {
-    val str = scala.io.StdIn.readLine("> ")
-    if (str != "quit") for {
-      parsed <- ExpressionParser.parse(str)
-      evaluated <- ExpressionEvaluator.eval(parsed, context)
-      result = evaluated match {
-        case (Bool(v), _) => v.toString
-        case (Number(v), _) => v.toString
-        case (Text(v), _) => v
-      }
-      name = s"res$count"
-      _ = println(s"$name := $parsed ( = $result)")
-      nctx = ExpressionEvaluator.put(name, parsed, context)
-    } yield repl(nctx, count + 1)
-  }
-
-  def compileFile(filename: String): Unit = {
-    val source = Source.fromFile(filename).getLines.mkString("\n")
-    println(source)
-    println
-    val result = for {
-      terms <- TermParser.parse(source)
-      rendered <- TermEvaluator.compile(terms, ExpressionEvaluator.EmptyContext)
-    } yield rendered
-    result match {
-      case Right(r) => println(r)
-      case Left(err) => println("An error occured")
-    }
-  }
 }
